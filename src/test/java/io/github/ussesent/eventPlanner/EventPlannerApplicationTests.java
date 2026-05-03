d57324481815db2ec89ec84dba7056d4e04b3709
ussesent <ussesent@mail.ru>
2026-05-03 14:52:11 +0300

    Initial commit

---
+++ src/test/java/io/github/ussesent/eventPlanner/EventPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ussesent.eventPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
